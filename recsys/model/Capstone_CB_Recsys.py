08519cdfb1d8be2af4767fd4566ea912cd9186e9
Jose <josebagus4@gmail.com>
2023-12-21 06:40:34 +0000

    hotfix: dockerfile, download punkt

--- recsys/model/Capstone_CB_Recsys.py
+++ recsys/model/Capstone_CB_Recsys.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import nltk
+nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_distances
